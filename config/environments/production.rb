d362d968cdf7d29f7b06e1aee84279dce864f1c1
Nishan Acharya <nishan.acharya@gmail.com>
2016-03-14 01:56:12 -0400

    Heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.action_mailer.default_url_options = { host: 'https://immense-sands-33738.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'peaceful-beyond-22393.herokuapp.com' }
 
   config.cache_classes = true
 
