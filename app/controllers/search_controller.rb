90514f3f1d77dac62d9f18c7dc559ea2c6bc7ef1
Nishan Acharya <nishan.acharya@gmail.com>
2016-03-21 03:46:30 -0400

    subject dropdown added and combined search implemented

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,14 @@
 class SearchController < ApplicationController
   def index
-    if params[:search]
-      @courses = Course.search(params[:search]).order("created_at DESC")
+    if !params[:subject].blank?
+      @courses = Subject.find(params[:subject]).courses
+      @courses = @courses.search(params[:search]).order(:Name)
+    else 
+      @courses = Course.search(params[:search]).order(:Name)
     end
+    
+    @subjects = Subject.all.order(:Name)
+
   end
 
   def new
