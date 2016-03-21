a3da6b317c5ba104d9c1131555d3a64046e3f8cc
Nishan Acharya <nishan.acharya@gmail.com>
2016-03-21 11:11:55 -0400

    postgres doesn't accept LIKE

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,6 +6,6 @@ class Course < ApplicationRecord
   has_many :subjects, through: :course_subjects
 
   def self.search(search)
-	where("name LIKE ?", "%#{search}%") 
+	where("name ILIKE ?", "%#{search}%") 
   end
 end
